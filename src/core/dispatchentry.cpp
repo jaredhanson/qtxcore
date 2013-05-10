254553818bb94e5babdba1e505a98f202ad0b2a3
Jared Hanson <jaredhanson@gmail.com>
2013-05-10 13:38:44 -0700

    Documentation for DispatchEntry.

--- src/core/dispatchentry.cpp
+++ src/core/dispatchentry.cpp
@@ -3,6 +3,19 @@
 QTX_BEGIN_NAMESPACE
 
 
+/*!
+    \class DispatchEntry
+    \inmodule QtxCore
+    \internal
+    
+    \brief The DispatchEntry class represents an entry in a NotificationCenter's
+           dispatch table.
+ */
+
+
+/*!
+    Constructs an entry for \a poster and \a notification to \a observer and \a method.
+*/
 DispatchEntry::DispatchEntry(const QObject *poster, const QString & notification, QObject *observer, const char *method)
     : mPoster(poster),
       mNotification(notification),
@@ -12,6 +25,9 @@ DispatchEntry::DispatchEntry(const QObject *poster, const QString & notification
     mMethod = qstrdup(method);
 }
 
+/*!
+    Destroys the entry.
+*/
 DispatchEntry::~DispatchEntry()
 {
     if (mMethod) {
@@ -20,21 +36,33 @@ DispatchEntry::~DispatchEntry()
     }
 }
 
+/*!
+    Returns the poster registered for this entry.
+*/
 const QObject *DispatchEntry::poster() const
 {
     return mPoster;
 }
 
+/*!
+    Returns the notification registered for this entry.
+*/
 QString DispatchEntry::notification() const
 {
     return mNotification;
 }
 
+/*!
+    Returns the observer registered for this entry.
+*/
 QObject *DispatchEntry::observer() const
 {
     return mObserver;
 }
 
+/*!
+    Returns the observer's method registered for this entry.
+*/
 const char *DispatchEntry::method() const
 {
     return mMethod;
